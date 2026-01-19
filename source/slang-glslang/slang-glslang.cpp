2067f30e96e1f29581d4137742da85f4d12cd56a
ye <654065929@qq.com>
2026-01-19 19:58:31 +0800

    test

--- source/slang-glslang/slang-glslang.cpp
+++ source/slang-glslang/slang-glslang.cpp
@@ -1,7 +1,7 @@
 // slang-glslang.cpp
 #include "slang-glslang.h"
 
-#include "SPIRV/GlslangToSpv.h"
+#include "glslang/SPIRV/GlslangToSpv.h"
 #include "glslang/Public/ShaderLang.h"
 #include "slang.h"
 #include "spirv-tools/libspirv.h"
